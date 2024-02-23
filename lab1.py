9add99dee6185ee5a5be5cd4cd15f3d9ddcff606
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-02-23 20:04:37 +1000

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -3,20 +3,24 @@ from scipy.optimize import linprog
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
+#размер матрицы цен
 I = int((input("Введите размер спроса:")))
 J = int((input("Введите размер предложения:")))
-cena = np.random.randint(100, size=(I, J)) #26
-
+#цены на товары
+cena = np.random.randint(100, size=(I, J))
+#матрицы в вектор
 m, n = cena.shape
 C = list(np.reshape(cena, n * m))
+#ограничения
 b1 = np.random.randint(100, size=I)
 b = np.random.randint(100, size=J)
+#ограничение предложений
 A = np.zeros([m, m * n])
 for i in range(n):
     for j in range(n * m):
         if i * n <= j <= n + i * n - 1:
             A[i, j] = 1
+#ограничение спроса
 A1 = np.zeros([n, m * n])
 for i in range(n):
     p = 0
@@ -24,6 +28,7 @@ for i in range(n):
         if j == p * n + i:
             A1[i, j] = 1
             p += 1
+#проверка баланса
 if np.sum(b1) < np.sum(b):
     res = linprog(c=C, A_ub=A1, b_ub=b, A_eq=A, b_eq=b1, method='HiGHS')
 elif np.sum(b1) > np.sum(b):
@@ -32,18 +37,20 @@ elif np.sum(b1) == np.sum(b):
     A_eq = np.concatenate((A, A1), axis=0)
     b_eq = np.concatenate((b1, b), axis=0)
     res = linprog(c=C, A_eq=A_eq, b_eq=b_eq, method='HiGHS')
-result = np.array(list(res['x'])).reshape(I, J)
-np.reshape(result, (21, 42))
-print(result)
+r = np.array(list(res['x'])).reshape(I, J)
+np.reshape(r, (I, J))
+print(r)
+#граф
 G = nx.Graph()
-variables = [f'x{i+1}' for i in range(I*J)]
-constraints = [f'c{i+1}' for i in range(I+J)]
-G.add_nodes_from(variables, bipartite=0)
-G.add_nodes_from(constraints, bipartite=1)
-edges = [(variables[i], constraints[j]) for i in range(I*J) for j in range(I+J) if res['x'][i] > 0]
-G.add_edges_from(edges)
-pos = nx.bipartite_layout(G, nodes=variables)
-nx.draw(G, pos, with_labels=True, node_color=['skyblue' if node in variables else 'salmon' for node in G.nodes])
-print(f"x = {result} \n \nfun = {res['fun']}, {res['message']}")
+spros = [f'D{i}' for i in range(I)]
+predloj = [f'S{i}' for i in range(J)]
+G.add_nodes_from(spros, bipartite=0)
+G.add_nodes_from(predloj, bipartite=1)
+for i in range(I):
+    for j in range(J):
+        G.add_edge(spros[i], predloj[j], weight=cena[i][j])
+res = nx.max_weight_matching(G)
+pos = nx.bipartite_layout(G, spros)
+nx.draw(G, pos, with_labels=True)
+nx.draw_networkx_edge_labels(G, pos, edge_labels={(u, v): w for u, v, w in G.edges.data('weight')})
 plt.show()
-
