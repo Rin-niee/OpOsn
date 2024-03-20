2040dba255b75999bd77578ee959cd8a299f10d7
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-03-20 17:01:41 +1000

    Update lab2.py

--- lab2.py
+++ lab2.py
@@ -5,21 +5,21 @@ from scipy.optimize import linprog
 
 #создание исходной матрицы +
                #1   #2  #3  #4  #5  #6   #7  #8    #9  #10 #11 #12  #13  #14  #15 #16  #17  #18
-D = np.array([[0,   0,  0,   5,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #1
+D = np.array([[0,   0,  0,  5,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #1
               [0,   0,  0,   0, 25,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0],  #2
               [0,   0,  0,   0,  0, 70,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #3
-              [0,   0,  0,   0,  0,  0,   5,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #4
+              [0,   0,  0,   0,  0,  0,  5,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #4
               [0,   0,  0,   0,  0,  0,   0, 25,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #5
               [0,   0,  0,   0,  0,  0,   0,  0,  70,  0,  0,  0,    0,   0,   0,   0,    0,   0], #6
-              [0,   0,  0,   0,  0,  0,   0,  0,   0,  5,  0,  0,    0,   0,   0,   0,    0,   0], #7
+              [0,   0,  0,   0,  0,  0,   0,  0,   0, 5,  0,  0,    0,   0,   0,   0,    0,   0], #7
               [0,   0,  0,   0,  0,  0,   0,  0,   0,  0, 25,  0,    0,   0,   0,   0,    0,   0], #8
               [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0, 70,    0,   0,   0,   0,    0,   0], #9
-              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    5,   0,   0,   0,    0,   0], #10
+              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,   5,   0,   0,   0,    0,   0], #10
               [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,   25,   0,   0,   0,    0,   0], #11
               [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,   70,   0,   0,   0,    0,   0], #12
-              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,  75,  60,   0,    0,   0], #13
-              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,  0,   18,   57,   0], #14
-              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,   40,  20], #15
+              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,  85,  50,   0,    0,   0], #13
+              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   35,  50,   0], #14
+              [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,   30,  20], #15
               [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #16
               [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0], #17
               [0,   0,  0,   0,  0,  0,   0,  0,   0,  0,  0,  0,    0,   0,   0,   0,    0,   0]]) #18
@@ -36,7 +36,6 @@ def matrix_incidence(x):
                 D1[i, j] = -1
     return D1
 D1 = matrix_incidence(D)
-print("incidence matrix: \n", D1)
 
 #подсчет количества ребер и вершин +
 edges_num = 0
@@ -46,17 +45,17 @@ for i in range(matrix_size):
         if D1[i, j] > 0:
          edges_num = edges_num + 1
 #рисование графа +
-def GraphDraw(x):
+def GraphDraw(x, x1):
     G = nx.DiGraph()
     for i in range(matrix_size):
         G.add_node(i)
         for j in range(matrix_size):
-            if (D1[i, j] > 0):
+            if (x[i, j] > 0):
                 G.add_edges_from([(i, j)])
-                G.add_edge(i, j, weight=D[i][j])
+                G.add_edge(i, j, weight=x[i][j])
     return G
 
-G = GraphDraw(D1)
+G = GraphDraw(D, D1)
 pos = nx.circular_layout(G)
 plt.figure(figsize=(8, 8))
 nx.draw(G, pos, with_labels=True)
@@ -76,7 +75,6 @@ def sinks(x3):
                     c[m] = 1
     return c
 c = sinks(D1)
-print("Celevaya: ", c)
 #матрица A_eq
 def conservation(x4):
     A_eq = np.zeros((matrix_size, matrix_size)) #создает матрицу смежности, если есть из в i -> j, то 1, наоборот -1
@@ -88,12 +86,14 @@ def conservation(x4):
                 A_eq[j, e] = -1
                 e+=1
     return A_eq
+
+#ограничения
 A_eq = conservation(D1)
 A_eq1 = A_eq[3:16]
-print("Matrix A_eq :\n", A_eq)
-
 b_eq = np.zeros(A_eq1.shape[0])
 
+
+#ограничения пропускной способности
 b_ub = np.zeros((edges_num))
 m = -1
 for i in range(matrix_size):
@@ -108,10 +108,11 @@ A_ub = np.eye(b_ub.size)
 # #Решение задачи линейного программирования
 res = linprog(-c, A_eq=A_eq1, b_eq=b_eq, A_ub=A_ub, b_ub=b_ub, method = "HiGHS")
 print(res)
-r = np.array(list(res['x']))
+r = np.array((res['x']))
 # Вывод максимального потока
 print(r)
 
+#рисование готового графа
 matrix = np.zeros((matrix_size, matrix_size))
 e = 0
 for i in range(matrix_size):
