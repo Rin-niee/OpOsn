b5c607b110153a8ed967243a95600c4b173041fa
Rin <125102825+Rin-niee@users.noreply.github.com>
2024-04-26 18:40:40 +1000

    Update lab4.py

--- lab4.py
+++ lab4.py
@@ -59,21 +59,20 @@ def A_eqcreate(K, M, N, L, Alk):
     # ∑𝑧𝑖𝑗𝑗=∑𝑧𝑗𝑖𝑗 (4)
     # ∑𝑧𝑖𝑁𝑖=∑𝑥𝑘𝑚𝑘,𝑚 (11)
     # 𝑏𝑙(𝑚+1)=𝑏𝑙𝑚−∑𝐴𝑙𝑘𝑥𝑘𝑚𝑘+𝛾𝑙𝑚 (12)
-    A_eq3 = [1]*(K*M) + [0]*(N**2) + [0] + [-1]*(N-1) + [0]*(N*(N-1)) + [0]*(L*M)
+    A_eq3 = [1]*(K*M) + [0]*(N**2) + [-1]*(N) + [0]*(N*(N-1)) + [0]*(L*M) #все верно
     A_eq.append(A_eq3)
-    A_eq4 = [[0] * N ** 2 for i in range(N)]
     for i in range(N):
         for j in range(N):
+            A_eq41 = [0] * (K * M + 2 * N ** 2)
             if D[i][j] != 0:
-                A_eq4[i][i*N + j] = 1
-                A_eq4[j][i*N + j] = -1
-
-    for i in range(N):
-        A_eq4[i] = [0] * (K + N ** 2) + A_eq4[i]
-    A_eq4 = A_eq4[1: N - 1]
-    A_eq.append(A_eq4)
+                A_eq41[K*M + (N**2) + i*N+j] = 1
+                A_eq41[K*M + (N**2) + j*N + i] = -1
+            A_eq41.extend([0] * (L * M))
+        if (i!=0 and i!=N):
+            A_eq.append(A_eq41)
     A_eq11 = [1]*(K*M) + [0]*(N**2) + ([0]*(N-1)+[-1])*N + [0]*(L*M)
     A_eq.append(A_eq11)
+    #тут должно быть 12 условие, но его украли цыгане, извините
     A_eq12 = []
     a1 = [1]*(K*M)
     a2 = [0]*(N**2) #лямбды
@@ -83,70 +82,60 @@ def A_eqcreate(K, M, N, L, Alk):
         for m in range(M-1):
             for k in range(K):
                 a1[k*M+m] = Alk[l][k]
-                a4[l*M+m]=-1
-                a4[l*M+(m-1)]=1
+                a4[l*M+m] = -1
+                a4[l*M+(m-1)] = 1
     A_eq12.extend(a1)
     A_eq12.extend(a2)
     A_eq12.extend(a3)
     A_eq12.extend(a4)
     A_eq.append(A_eq12)
     return A_eq
-
 def b_eqcreate(N, Ylm, L, M):
-    b_eq = [0]*(1+N-2+1)
+    b_eq = []
+    b_eq+=[0]
+    b_eq+=[0]*(N-2)
+    b_eq+=[0]
     b1 = np.reshape(Ylm, L*M)
-    b_eq.extend(b1)
+    b1 = b1[0:M]
+    b_eq.extend(list(b1))
     return b_eq
 A_eq = A_eqcreate(K, M, N, L, Alk)
 b_eq = b_eqcreate(N, Ylm, L, M)
+
+print(len(A_eq), len(b_eq))
 A_ub = []
-b_ub = []
 #2
 for l in range(L):
-    A_ub.append(list(Alk[l]) + [0]*2*(N**2)+[-1]*(L*M))
-b_ub += 0
+    for m in range(M):
+        aa = [0] * (K * M)
+        bb = [0] * (L * M)
+        for k in range(K):
+            aa[k * M + m] = Alk[l][k]
+            bb[l*M+m] = -1
+        A_ub.append(aa + [0]*(2*(N**2)) + bb)
+#7
+for i in range(N):
+    for j in range(N):
+        aoq = [0] * (K*M + 2 * N**2 + M*L)
+        # A_ub.append([0] * (K*M + 2 * N ** 2 + M*L))
+        aoq[K*M + i * N + j] = -1000
+        aoq[K*M + N ** 2 + i * N + j] = 1
+        A_ub.append(aoq)
 #5
 for k in range(K):
-    A_ub.append([1]*(K*M) + [0]*(2*(N**2)) + [0]*(L*M))
-b_ub += list(Qk)
-#6
-for j in range(N**2):
-    A_ub.append([0] * (K + 2*N**2))
-    A_ub[-1][K + N**2 + j] = 1
-A_ub+=[0]*(M*L)
-b_ub += list(D.flatten())
-#7
-for j in range(N ** 2):
-    A_ub.append([0] * (K + 2 * N ** 2))
-    A_ub[-1][K + j] = -10
-    A_ub[-1][K + N ** 2 + j] = 1
-A_ub+=[0]*(M*L)
-b_ub += [0] * (N ** 2)
-#9
-for j in range(N**2):
-    A_ub.append([0] * (K + M*L + 2*N**2))
-    A_ub[-1][K + j] = 1
-
-b_ub += [1] * (N ** 2)
-
-# res = linprog(c=C, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq, integrality=np.ones(K+2*N**2))
-#
-#
-# def vs(matrix1, matrix2, matrix3=[]):
-#     G = nx.DiGraph(matrix1)
-#     pos = nx.spring_layout(G)
-#     nx.draw(G, pos, with_labels=True, node_size=900, node_color="lightblue")
-#     edge_labels = {}
-#     for i, j, w in G.edges(data=True):
-#         label = f'{w["weight"] if w["weight"] != 0 else 0} , {matrix2[i, j]}'
-#         if len(matrix3) > 0:
-#             label += f' | {matrix3[i, j]}'
-#         edge_labels[(i, j)] = label
-#     nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_color='blue',font_size=7)  # Рисуем подписи на ребрах
-#     plt.show()
-# vs(D, Cij)
-# b = res.x
-# b = b[-(len(Cij) ** 2):].reshape(len(Cij), len(Cij))
-# print(b, "\nGraph ======\n", D, "\nResolution======\n", res.x)
-#
-# vs(b, D, Cij)  # Визуализация доставки товаров,  пропускной способности, стоимости перевозки
+    aaaaa = [0] * (K * M)
+    for m in range(M):
+        aaaaa[k*M + m] = 1
+    A_ub.append(aaaaa + [0]*(2*(N**2)) + [0]*(L*M))
+def b_ubcreate(N, L, Qk):
+    b_ub=[]
+    b_ub.extend([0]*(L*M))
+    b_ub.extend([0] * (N ** 2))
+    b_ub.extend(list(Qk))
+    return b_ub
+b_ub = b_ubcreate(N, L, Qk)
+print(b_ub)
+# bounds_lower = np.concatenate((np.zeros(K), np.zeros(L), np.zeros(L)))
+# bounds_upper = np.concatenate((Qk, np.inf * np.ones(L), np.inf * np.ones(L)))
+res = linprog(c=C, A_ub=A_ub, b_ub=b_ub, A_eq=A_eq, b_eq=b_eq)
+print(res)
